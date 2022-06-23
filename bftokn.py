c63448d6f5744d348fe1ffdc774f02e9a45d387f
ark231 <arklp20909@gmail.com>
2022-06-24 04:08:01 +0900

    add: support for generating CMakeLists.txt

--- bftokn.py
+++ bftokn.py
@@ -2,6 +2,9 @@
 
 import argparse
 import sys
+from pathlib import Path
+import shutil
+import subprocess
 
 
 def main():
@@ -19,10 +22,10 @@ def main():
     args = parser.parse_args()
 
     if args.infile is None:
-        input = sys.stdin.read()
+        source = sys.stdin.read()
     else:
         with open(args.infile, "r") as infile:
-            input = infile.read()
+            source = infile.read()
     result = ""
 
     if args.use_macro:
@@ -32,7 +35,7 @@ def main():
     else:
         section_start = "::kcufniarb::kcufniarB()"
 
-    for c in reversed(input):
+    for c in reversed(source):
         if c == ">":
             result += "!"
         elif c == "<":
@@ -79,11 +82,58 @@ int main(){{
     {result}
 }}\
         """
+    cmake_result = f"""\
+cmake_minimum_required(VERSION 3.23.2)
+project({Path(args.infile).stem if args.infile is not None else "unnamed"} CXX)
+
+# include(ExternalProject)
+# ExternalProject_Add(
+#     ppc_kcufniarb_git
+#     GIT_REPOSITORY git@github.com:ark231/ppc_kcufniarb.git
+#     GIT_TAG {"bracket" if args.loop_type == "operator-bracket" else "main"}
+#     GIT_PROGRESS ON
+# )
+# add_subdirectory(${{CMAKE_CURRENT_BINARY_DIR}}/ppc_kcufniarb_git-prefix/src/ppc_kcufniarb_git)
+add_subdirectory(${{CMAKE_CURRENT_SOURCE_DIR}}/ppc_kcufniarb)
+
+add_executable({Path(args.infile).stem if args.infile is not None else "unnamed"} {args.outfile})
+target_link_libraries({Path(args.infile).stem if args.infile is not None else "unnamed"} PRIVATE ppc_kcufniarb)\
+    """
     if args.outfile is None:
         print(result)
+        if "cmake" in args.output_type:
+            print("\n/*=========================================*/\n")
+            print(cmake_result)
     else:
         with open(args.outfile, "w") as outfile:
             outfile.write(result)
+        if "cmake" in args.output_type:
+            cmake_filepath = Path(args.outfile).parent / "CMakeLists.txt"
+            write_output = True
+            if cmake_filepath.exists():
+                answer = input(f"file {cmake_filepath} exists. Do you Want to overwrite it?[y/N] ")
+                if answer.lower() != "y":
+                    write_output = False
+            if write_output:
+                with open(cmake_filepath, "w") as cmakefile:
+                    cmakefile.write(cmake_result)
+                repo_path = cmake_filepath.parent / "ppc_kcufniarb"
+                if repo_path.exists():
+                    answer = input(f"repository {repo_path} exists. Do you Want to overwrite it?[y/N] ")
+                    if answer.lower() == "y":
+                        shutil.rmtree(repo_path)
+
+                subprocess.run(
+                    [
+                        "git",
+                        "clone",
+                        "git@github.com:ark231/ppc_kcufniarb.git",
+                        repo_path,
+                        "-b",
+                        "bracket" if args.loop_type == "operator-bracket" else "main",
+                    ],
+                    check=True,
+                )
 
 
 if __name__ == "__main__":
